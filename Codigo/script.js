f06af17c320b20473b248e715c5be2c6a2e7ffe2
guvdutra <guvadutra@gmail.com>
2023-12-04 22:48:08 -0300

    confirmação de cpf

--- Codigo/script.js
+++ Codigo/script.js
@@ -18,6 +18,8 @@ new Vue({
         this.formaPagamento === ''
       ) {
         alert('Por favor, preencha todos os campos.');
+      } else if (!this.validarCPF(this.cpf)) {
+        alert('Por favor, insira um CPF válido.');
       } else {
         //passando os campos por url 
         var url = `confirmacao.html?nome=${this.nome}&email=${this.email}&dataNascimento=${this.dataNascimento}&cpf=${this.cpf}&formaPagamento=${this.formaPagamento}`;
@@ -29,7 +31,13 @@ new Vue({
         
         //console.log('Formulário submetido!', this.nome, this.email, this.data, this.cpf, this.selectedPayments);
       }
+    },
+    validarCPF(cpf) {
+      // Expressão regular para validar CPF
+      const cpfRegex = /^[0-9]{11}$/;
+
+      // Verifica se o CPF possui 11 dígitos e atende à expressão regular
+      return cpfRegex.test(cpf);
     }
   }
 });
-
